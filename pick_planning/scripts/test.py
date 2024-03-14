8e0f943595afd4bfd0b56c989c8820c59402eb7d
keegan <keegan.nave@gmail.com>
2024-03-14 00:11:35 -0700

    visual servoing working with RGBD camera, can succesfully move to goals

--- pick_planning/scripts/test.py
+++ pick_planning/scripts/test.py
@@ -11,22 +11,56 @@ from tf2_ros.transform_listener import TransformListener
 from tf2_ros import TransformException
 from geometry_msgs.msg import TwistStamped, PoseStamped, Vector3, Pose
 import tf2_geometry_msgs
+import math
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from std_srvs.srv import Trigger
+from controller_manager_msgs.srv import SwitchController
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(Image,'/camera2/image_raw',self.listener_callback,10)
+        # self.subscription = self.create_subscription(Image,'/camera2/image_raw',self.listener_callback,10)
+        self.subscription = Subscriber(self,Image,'/camera2/image_raw')
+        self.depth_sub = Subscriber(self,Image,'/camera2/depth/image_raw')
         self.servo_publisher = self.create_publisher(TwistStamped, "/servo_node/delta_twist_cmds", 10)
-        self.max_vel = 0.8
-        self.br = CvBridge()
+        self.cli = self.create_client(Trigger, "/servo_node/start_servo")
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("service not available, waiting again...")
+        activated = self.activate_servo_node()
 
+        self.switch_controller_client = self.create_client(SwitchController, "/controller_manager/switch_controller")
+        while not self.switch_controller_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("service not available, waiting again...")
+        activated2 = self.switch_controller()
+        print("LSDKFJSDLKFJSDFLKJ2", activated2)
+
+        self.max_vel = 0.8
+        self.smoothing_factor = 12
+        self.br = CvBridge()
+        queue_size = 30
+        self.ts = ApproximateTimeSynchronizer([self.subscription, self.depth_sub],queue_size,0.01,)
+        self.ts.registerCallback(self.listener_callback)
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
+
+    def switch_controller(self):
+        self.request = SwitchController.Request()
+        self.request.activate_controllers = ["forward_position_controller"]
+        self.request.deactivate_controllers = ["joint_trajectory_controller"]
+        self.future = self.switch_controller_client.call_async(self.request)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
+    def activate_servo_node(self):
+        self.request = Trigger.Request()
+        self.future = self.cli.call_async(self.request)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
     def normalize(self, val, minimum, maximum):
         return (val - minimum) / (maximum-minimum)
-
 
     def transform_optical_to_ee(self, x, y):
         origin = PoseStamped()
@@ -41,27 +75,25 @@ class MinimalSubscriber(Node):
         new_pose = self.tf_buffer.transform(origin, "tool0", rclpy.duration.Duration(seconds=1))
         return new_pose
 
+    def exponential_vel(self, vel):
+        return (1-np.exp((-self.smoothing_factor/2)*vel)) * self.max_vel
 
-    def create_servo_vector(self, closest_apple, image):
+    def create_servo_vector(self, closest_apple, image, distance):
         apple_x = closest_apple[0]
         apple_y = closest_apple[1]
         center_x = image.shape[1] // 2
         center_y = image.shape[0] // 2
         # get x magnitude
         if apple_x >= center_x:
-            new_x = self.normalize(apple_x, center_x, image.shape[1])
+            new_x = self.exponential_vel(self.normalize(apple_x, center_x, image.shape[1]))
         else:
-            new_x = self.normalize(apple_x, 0, center_x)
+            new_x = -self.exponential_vel(1-self.normalize(apple_x, 0, center_x))
         # get y magnitude
         if apple_y >= center_y:
-            new_y = self.normalize(apple_y, center_y, image.shape[0])
+            new_y = self.exponential_vel(self.normalize(apple_y, center_y, image.shape[0]))
         else:
-            new_y = self.normalize(apple_y, 0, center_y)
+            new_y = -self.exponential_vel(1 - self.normalize(apple_y, 0, center_y))
         
-        new_x *= self.max_vel 
-        new_y *= self.max_vel 
-
-        print(new_x, new_y)
         transformed_vector = self.transform_optical_to_ee(new_x, new_y)
 
         vel_vec = TwistStamped()
@@ -69,13 +101,17 @@ class MinimalSubscriber(Node):
         vel_vec.header.frame_id = "tool0"
         vel_vec.twist.linear.x = transformed_vector.pose.position.x
         vel_vec.twist.linear.y = transformed_vector.pose.position.y
-        vel_vec.twist.linear.z = 0.0
+        if distance < .05:
+            vel_vec.twist.linear.z = 0.0
+        else:
+            vel_vec.twist.linear.z = 0.3
 
         return vel_vec
 
 
-    def listener_callback(self, msg):
-        print("In callback")
+    def listener_callback(self, msg, msg2):
+        depth = self.br.imgmsg_to_cv2(msg2, "32FC1")
+        # depth = cv2.normalize(depth, depth, 0,1,cv2.NORM_MINMAX)
 
         image = self.br.imgmsg_to_cv2(msg, "bgr8")
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -86,23 +122,35 @@ class MinimalSubscriber(Node):
         mask = cv2.inRange(hsv, lower_red, upper_red)
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         apple_centers = []
+        xy_dist = []
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             apple_centers.append([x+w//2, y+h//2])
+            xy_dist.append(math.sqrt((image.shape[1]//2 - x+w//2)**2 + (image.shape[0]//2 - y+h//2)**2))
 
         if apple_centers:
-            closest_apple = min(apple_centers)
+            closest_contour = contours[np.argmin(xy_dist)]
+            mask = np.zeros_like(depth)
+            cv2.drawContours(mask, [closest_contour], 0, (255), -1)
+            dist_to_apple = np.average(depth[np.nonzero(mask == 255)])
+            closest_apple = apple_centers[np.argmin(xy_dist)]
+            cv2.circle(image, (closest_apple[0], closest_apple[1]), 5, (0, 255, 0), 2)
+            print("DISTANCE", dist_to_apple)
+            print(np.argmin(xy_dist))
 
             try:
-                vec = self.create_servo_vector(closest_apple, image)
+                vec = self.create_servo_vector(closest_apple, image, dist_to_apple)
                 self.servo_publisher.publish(vec)
             except TransformException as e:
                 self.get_logger().info(f'Transform failed: {e}')
 
 
-        # cv2.imshow("camera", image)
-        # cv2.waitKey(1)
+        cv2.imshow("camera", image)
+        cv2.waitKey(1)
+
+        cv2.imshow("mask", depth)
+        cv2.waitKey(1)
 
 def main(args=None):
     rclpy.init(args=args)
